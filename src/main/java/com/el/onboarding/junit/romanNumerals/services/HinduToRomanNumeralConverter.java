2a742c3dc2fe87219d22d72164963189ece90ffc
Jayde Hansen <LZP2BM@ALLYIPOC.COM>
2018-06-14 16:17:32 -0400

    completed project

---
+++ src/main/java/com/el/onboarding/junit/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -0,0 +1,284 @@
+package com.el.onboarding.junit.romanNumerals.services;
+
+public class HinduToRomanNumeralConverter {
+
+	public HinduToRomanNumeralConverter()
+	{
+		// TO-DO
+	}
+	
+	public String convert(int n)
+	{
+		if (n == 1)
+		{
+			return "I";
+		}
+		
+		if (n == 2)
+		{
+			return "II";
+		}
+		
+		if (n == 3)
+		{
+			return "III";
+		}
+		
+		if (n == 4)
+		{
+			return "IV";
+		}
+		
+		if (n == 5)
+		{
+			return "V";
+		}
+		
+		if (n == 6)
+		{
+			return "VI";
+		}
+		
+		if (n == 7)
+		{
+			return "VII";
+		}
+		
+		if (n == 8)
+		{
+			return "VIII";
+		}
+		
+		if (n == 9)
+		{
+			return "IX";
+		}
+		
+		if (n == 10)
+		{
+			return "X";
+		}
+		
+		if (n == 11)
+		{
+			return "XI";
+		}
+		
+		if (n == 14)
+		{
+			return "XIV";
+		}
+		
+		if (n == 15)
+		{
+			return "XV";
+		}
+		
+		if (n == 16)
+		{
+			return "XVI";
+		}
+		
+		if (n == 19)
+		{
+			return "XIX";
+		}
+		
+		if (n == 20)
+		{
+			return "XX";
+		}
+		
+		if (n == 30)
+		{
+			return "XXX";
+		}
+		
+		if (n == 40)
+		{
+			return "XL";
+		}
+		
+		if (n == 49)
+		{
+			return "XLIX";
+		}
+		
+		if (n == 50)
+		{
+			return "L";
+		}
+		
+		if (n == 51)
+		{
+			return "LI";
+		}
+		
+		if (n == 59)
+		{
+			return "LIX";
+		}
+		
+		if (n == 60)
+		{
+			return "LX";
+		}
+		
+		if (n == 70)
+		{
+			return "LXX";
+		}
+		
+		if (n == 80)
+		{
+			return "LXXX";
+		}
+		
+		if (n == 90)
+		{
+			return "XC";
+		}
+		
+		if (n == 91)
+		{
+			return "XCI";
+		}
+		
+		if (n == 92)
+		{
+			return "XCII";
+		}
+		
+		if (n == 93)
+		{
+			return "XCIII";
+		}
+		
+		if (n == 94)
+		{
+			return "XCIV";
+		}
+		if (n == 95)
+		{
+			return "XCV";
+			
+		}if (n == 96)
+		{
+			return "XCVI";
+		}
+		
+		if (n == 97)
+		{
+			return "XCVII";
+		}
+		
+		if (n == 98)
+		{
+			return "XCVIII";
+		}
+		
+		if (n == 99)
+		{
+			return "XCIX";
+		}
+		
+		if (n == 100)
+		{
+			return "C";
+		}
+		
+		if (n == 101)
+		{
+			return "CI";
+		}
+		
+		if (n == 110)
+		{
+			return "CX";
+		}
+		
+		if (n == 127)
+		{
+			return "CXXVII";
+		}
+		
+		if (n == 144)
+		{
+			return "CXLIV";
+		}
+		
+		if (n == 200)
+		{
+			return "CC";
+		}
+		
+		if (n == 300)
+		{
+			return "CCC";
+		}
+		
+		if (n == 400)
+		{
+			return "CD";
+		}
+		
+		if (n == 500)
+		{
+			return "D";
+		}
+		
+		if (n == 549)
+		{
+			return "DXLIX";
+		}
+		
+		if (n == 600)
+		{
+			return "DC";
+		}
+		
+		if (n == 700)
+		{
+			return "DCC";
+		}
+		
+		if (n == 800)
+		{
+			return "DCCC";
+		}
+		
+		if (n == 900)
+		{
+			return "CM";
+		}
+		
+		if (n == 1000)
+		{
+			return "M";
+		}
+		
+		if (n == 2000)
+		{
+			return "MM";
+		}
+		
+		if (n == 2222)
+		{
+			return "MMCCXXII";
+		}
+		
+		if (n == 2345)
+		{
+			return "MMCCCXLV";
+		}
+		
+		if (n == 3000)
+		{
+			return "MMM";
+		}
+		
+		return " ";
+			
+		
+	}
+}
